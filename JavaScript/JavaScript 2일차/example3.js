3e52163ac931aae2f20392a8ba1da5d5bb101535
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-07 21:19:39 +0900

    Create example3.js

---
+++ JavaScript/JavaScript 2일차/example3.js
@@ -0,0 +1,12 @@
+//외부 자바스크립트 파일로 생성
+//아래 코드를 별도의 파일로 생성 example.js
+function printDate() {
+  document.getElementByld('date').innerHTML = Date();
+}
+
+/*
+<head>
+  <meta charset='UTF-8'>
+  <title>JavaScript Apply</title>
+  <script src='/examples/media/example.js'></script> 폴더에서 파일 불러오기
+</head>
