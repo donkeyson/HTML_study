841fcb575a1bf5e9df2cd41fbf8760ae3c259169
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-10 20:26:40 +0900

    Update rest_parameter.js

--- JavaScript/JavaScript 5일차/rest_parameter.js
+++ JavaScript/JavaScript 5일차/rest_parameter.js
@@ -1,8 +1,7 @@
 //나머지 매개변수(rest parameter)
-function sub() {
-  var firstNum = arguments[0]; //첫번째 인수
-  for(var i = 0; i < arguments.length-1; i++) { //두 번째부터 마지막 인수까지
-    firstNum -= arguments[i+1]; //빼기
+function sub(firstNum, ...restArgs) { //첫 번째 인수만 firstNum으로 따로 저장, 나머지는 restArgs로 저장
+  for(var i = 0; i < restArgs.length; i++) { //두 번째부터 마지막 인수까지
+    firstNum -= restArgs[i]; //빼기
   }
   return firstNum;
 }
