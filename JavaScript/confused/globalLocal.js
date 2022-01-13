28b29e2011fed0338829862b8c40459c841728ad
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-13 19:22:14 +0900

    Create globalLocal.js

---
+++ JavaScript/confused/globalLocal.js
@@ -0,0 +1,9 @@
+var outerWear = 'T-Shirt'; //global
+
+function myOutfit() {
+  var outerWear = 'sweater';
+  return outerWear;
+}
+
+console.log(myOutfit()); //sweater
+console.log(outerWear); //T-Shirt
