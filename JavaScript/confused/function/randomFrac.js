a53104f579e3d1f11cfb692be4287237b49f3104
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-14 19:07:29 +0900

    Create randomFrac.js

---
+++ JavaScript/confused/function/randomFrac.js
@@ -0,0 +1,5 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20) //not including 20
+
+function randomFraction() {
+  return Math.floor(Math.random() * 10); //between 0~9 not including 10
+}
